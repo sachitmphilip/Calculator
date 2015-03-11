095cc8f2461afa86bceb356f98212566e2170fd5
Sachit <sachitmphilip@yahoo.com>
2015-03-11 15:55:24 +0530

    first commit

--- Calculator/ViewController.h
+++ Calculator/ViewController.h
@@ -6,8 +6,23 @@
 //  Copyright (c) 2012 enterpi. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    float result;
+    IBOutlet UILabel *calculatorScreen;
+    NSInteger currentOperaton;
+    float currentNumber;
+    
+}
+@property(nonatomic,strong)    IBOutlet UILabel *calculatorScreen;
+
+
+-(IBAction)buttonDigitPressed:(id)sender;
+-(IBAction)buttonOperationPressed:(id)sender;
+-(IBAction)cancelInput;
+-(IBAction)cancelOperation;
 
 @end
--- Calculator/ViewController.m
+++ Calculator/ViewController.m
@@ -13,6 +13,67 @@
 @end
 
 @implementation ViewController
+@synthesize calculatorScreen;
+
+-(IBAction)buttonDigitPressed:(id)sender
+{
+    currentNumber = currentNumber *10 + (float)[sender tag];
+    calculatorScreen.text = [NSString stringWithFormat:@"%2f",currentNumber];
+}
+
+-(IBAction)buttonOperationPressed:(id)sender
+{
+    if (currentOperaton == 0)result = currentNumber;
+    else {
+        switch (currentOperaton) {
+            case 1:
+                result = result + currentNumber;
+                break;
+            case 2:
+                result = result - currentNumber;
+                break;
+            case 3:
+                result = result * currentNumber;
+                break;
+            case 4:
+                result = result / currentNumber;
+                break;
+            case 5:
+                currentOperaton = 0;
+                break;
+                
+                
+        }
+    }
+    currentNumber = 0;
+    calculatorScreen.text = [NSString stringWithFormat:@"%2f",result];
+    if ([sender tag]==0) result = 0;
+    currentOperaton = [sender tag];
+    
+    
+    
+}
+
+
+-(IBAction)cancelInput
+{
+    currentNumber = 0;
+    calculatorScreen.text = @"0";
+    
+}
+-(IBAction)cancelOperation
+
+{
+    currentNumber = 0;
+    calculatorScreen.text = @"0";
+    currentOperaton = 0;
+    
+    
+}
+
+
+
+
 
 - (void)viewDidLoad
 {
